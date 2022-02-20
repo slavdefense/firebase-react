390a0d3da4252c19776957a2017d11cc8fe012c2
Sagun Shrestha <goodsamaritan.4846@gmail.com>
2022-02-20 12:14:18 -0800

    Fetch data through firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from "react";
 
-import MoviesList from './components/MoviesList';
-import AddMovie from './components/AddMovie';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
+import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -12,35 +12,80 @@ function App() {
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
+
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch(
+        "https://react-http-882c3-default-rtdb.firebaseio.com/movies.json"
+      );
+      
       if (!response.ok) {
-        throw new Error('Something went wrong!');
+        throw new Error("Something went wrong!");
       }
 
       const data = await response.json();
+      console.log(data)
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      let myArr = []
+      for(let item in data){
+        myArr.push( {
+          key: item,
+          title: data[item].title,
+          release:data[item].releaseDate,
+          openingText: data[item].openingText
+
+        })
+      }
+      console.log(myArr)
+     setMovies(myArr)
+
+
+
+      // const transformedMovies = movies.map((movieData) => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_date,
+      //   };
+
+      // });
+      // console.log(transformedMovies)
+     
     } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
   }, []);
+  
 
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+  
+
+    try {
+      const response = await fetch(
+        "https://react-http-882c3-default-rtdb.firebaseio.com/movies.json",
+        {
+          method: "POST",
+          body: JSON.stringify(movie),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (!response.ok) {
+        throw new Error("something happened");
+      }
+      const data = response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+
+
   }
 
   let content = <p>Found no movies.</p>;
